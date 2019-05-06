129e2185c181b16bd1d5c8e199927d4f3fd48a0f
parango26 <pabloarangobte@gmail.com>
2019-05-05 19:27:32 -0500

    commit

---
+++ helloworld.py
@@ -0,0 +1,33 @@
+print("Hola mundo")
+
+# Number
+type(30)
+type(30.5)
+
+# Integer
+print(type(30))
+
+# float
+print(type(30.5))
+
+# Boolean
+print(type(True))
+print(type(False))
+
+# List
+numeros_suerte = [7, 13, 23, 33]
+saludos = ["Hola", "Hello", "Holi", "Oelo"]
+[]
+
+# Tuples
+(10, 20, 30)
+()
+print(type((10, 20, 30)))
+
+# Dictionaries
+{
+    "Name": "Pablo",
+    "Apellido": "Arango",
+    "Apellido2": "Bustamante"
+}
+
